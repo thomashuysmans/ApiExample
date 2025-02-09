baf64f7f3f4af646c6965aec313f35a873b1c002
Thomas Huysmans <thomash@Yharnam.localdomain>
2025-02-09 10:46:52 +0100

    Init

---
+++ ApiExample/Controllers/UserSettingsController.cs
@@ -0,0 +1,89 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiExample.Controllers;
+
+[ApiController]
+[Route("/user-settings")]
+public class UserSettingsController : ControllerBase
+{
+    
+    [HttpGet]
+    [Route("{userId}")]
+    public IActionResult Get([FromRoute] int userId)
+    {
+
+        if (userId == 1)
+        {
+            var userSettings = new UserSettings
+            {
+                Id = 1,
+                ScreenSetting = ScreenSetting.Dark
+            };
+
+            return Ok(new ApiResponse()
+            {
+                Data = userSettings
+            });    
+        }
+
+        var problemDetails = new ProblemDetails()
+        {
+            Status = 400,
+            Title = "User not found",
+            Detail = "User with id " + userId + " not found"
+        };
+
+        return StatusCode(problemDetails.Status.Value, problemDetails);
+    }
+    
+    [HttpPost]
+    public IActionResult Post([FromBody] UserSettings userSettings)
+    {
+        if (userSettings == null)
+        {
+            return BadRequest(new ProblemDetails()
+            {
+                Status = 400,
+                Title = "Invalid request",
+                Detail = "User settings are required"
+            });
+        }
+        
+        return Ok(new ApiResponse()
+        {
+            Data = userSettings
+        });
+    }
+}
+
+public record UserSettings
+{
+    public int Id { get; set; }
+    
+    public ScreenSetting ScreenSetting { get; set; } 
+}
+
+public enum ScreenSetting
+{
+    Light,
+    Dark
+}
+
+
+public record ApiResponse
+{
+    public object Data { get; set; }
+    
+    
+}
+
+public record ApiErrorResponse
+{
+    public string Status { get; set; }
+    
+    public string Code { get; set; }
+    
+    public string Title { get; set; }
+    
+    public string Detail { get; set; }
+}
---
+++ ApiExample/Program.cs
@@ -0,0 +1,29 @@
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
+builder.Services.AddProblemDetails();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
